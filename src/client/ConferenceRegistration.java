4a4f4d78cbd016c3bb848fe80cb191236f9bf94d
graziaFerrara <grazia.ferrara2000@gmail.com>
2023-10-30 17:31:37 +0100

    first client commit

---
+++ src/client/ConferenceRegistration.java
@@ -0,0 +1,14 @@
+package client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ConferenceRegistration extends Remote{
+	
+	public boolean register(String name, int day, int session) throws RemoteException;
+	
+	public String [][][] getInformation() throws RemoteException;
+	
+	public boolean cancelRegistration(String name, int day, int session) throws RemoteException;
+
+}
